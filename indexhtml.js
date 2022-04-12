f6b190560e45deb6eac687acf1d4df19eab0c66c
NullTxt <90879949+NullTxt@users.noreply.github.com>
2022-04-12 15:21:38 -0700

    Update indexhtml.js

--- indexhtml.js
+++ indexhtml.js
@@ -7,7 +7,7 @@ function readFileA () {
     reader.addEventListener("loadend", () => {
         //get line 6 from file reader
         let line6 = reader.result.split("\n")[4];
-        if(line6 =='     <input type="button" value="Click Me">') {
+        if(line6 =='<input type="button" value="Click Me">') {
           document.getElementById("sf").innerHTML = "You finished";
         } else {
           document.getElementById("sf").innerHTML = "That was close but not quite";
